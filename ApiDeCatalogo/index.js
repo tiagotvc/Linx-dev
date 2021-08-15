c40526ef78a569e6136c4538edfeac660592d026
Tiago Vinicius Machado Carvalho <49541438+tiagotvc@users.noreply.github.com>
2021-08-15 03:32:08 -0300

    Criado index.js e conexão com o mongo DB

---
+++ ApiDeCatalogo/index.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const cors = require('cors')
+const productRouter = require('./routes/product-router')
+const db = require('./db')
+const app = express()
+
+app.use(cors());
+
+app.options('*', cors());
+
+app.use(express.json({
+    limit: "20mb"
+}));
+
+app.use(express.urlencoded({
+    limit: "20mb",
+    extended: true
+}));
+
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+app.use('/api', productRouter)
+
+
+const port = process.env.PORT || 3001;
+module.exports = app.listen(port, () => console.log(`Listening on port ${port}....`));
