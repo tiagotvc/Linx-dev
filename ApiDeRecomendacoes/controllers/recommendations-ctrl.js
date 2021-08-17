5f80cde276bc4eef0c4c755d17a35efcdc47f12a
Tiago Vinicius Machado Carvalho <49541438+tiagotvc@users.noreply.github.com>
2021-08-17 16:30:56 -0300

    Feito refactor na funcao de cache

--- ApiDeRecomendacoes/controllers/recommendations-ctrl.js
+++ ApiDeRecomendacoes/controllers/recommendations-ctrl.js
@@ -9,124 +9,156 @@
     //const redis = require('../cache/redis-client');
 
 const axios = require('axios');
-let first = 'undefined';
-let second = 'undefined';
-let cache = false;
 const redis = require('../cache/redis-client');
-   
+let firsts;
+let first = '';
+let second = '';
+let seconds;
+let allProduct;
+let allProducts = '';
+let cache = false;
+
+ 
 async function cacheInitialize(){
 
+     /** Consulta a Api de Catalogo, mas sem ID, para tornar o processo mais veloz
+     * optei por trazer a lista inteira de produtos e guarda-la em cache, dessa
+     * forma é evitado que precise ser feito uma busca por cada id dentro da API,
+     * tentei fazer dessa forma mas é simplesmente inviavel. Para funcionar criei
+     * um novo type chamado microservice e passo o id como 0 (poderia ser qualquer id
+     * pois o campo que será o identificador é  o type) ** Cabe uma melhoria aqui
+     * mas o tempo está curto.
+     */
+
+    let type = 'microservice';
+
+    const info = await axios.get('http://localhost:3001/api/products/'+ 0 + '/' + type)
+
+    await redis.setAsync(allProducts, JSON.stringify(info.data));
+    await redis.exAsync(allProducts, 1200);
+    allProduct = await redis.getAsync(allProducts);
+
+
+
     /**Consulta as duas listas de id */
-
+    
+    
     const fisrtList = await axios.get('https://wishlist.neemu.com/onsite/impulse-core/ranking/mostpopular.json');
-    const secondList = await axios.get('https://wishlist.neemu.com/onsite/impulse-core/ranking/pricereduction.json');
-
-    let lists = [];
-
-    /**Salva as listas no cache */
-
     await redis.setAsync(first, JSON.stringify(fisrtList.data));
-    await redis.setAsync(second, JSON.stringify(secondList.data));
-
-    /**Seta o tempo que as listas ficarão no cache */
-
     await redis.exAsync(first, 1200);
+    firsts = await redis.getAsync(first);
+
+
+
+    const secondList = await axios.get('https://wishlist.neemu.com/onsite/impulse-core/ranking/pricereduction.json');
+    await redis.setAsync(second, JSON.stringify(secondList.data));
     await redis.exAsync(second, 1200);
+    seconds = await redis.getAsync(second);
 
-    lists.push(fisrtList.data);
-    lists.push(secondList.data);
 
-    return lists
+    
+    return "done";
 }
 
     
 getRecommendations = async (req, res) => {
 
+    /**Bloco responsável por limpar o cache caso o sistema seja reiniciado
+     * apenas para garantir que nada fique no cache quando o sistema passar 
+     * por um reboot.
+     */
+
     if(!cache){
         redis.delAsync(first);
         redis.delAsync(second);
-        cache = true;
+        redis.delAsync(allProducts);
+        
+        await cacheInitialize();
     
         //Mantive o console pois ele indica quando o cache é limpo!
         console.log("Clean cache!");
+
     }
+
+
+    console.log(allProduct)
+
+
+
+    /** A const validade retorna o valor salvo em cache de produtos,
+     * abaixo dele é feito uma validação se existem dados nele, caso
+     * sim a variavel products recebe o valor dele em forma de JSON
+     * caso não tenha valor é chamado a função que consulta a API de
+     * catalogo.
+     */
+
+    
+
+
+
+    
 
       
     //const {maxItens, type} = query;
 
-    let firstList = JSON.parse(await redis.getAsync(first));
-    let secondList = JSON.parse(await redis.getAsync(second));
+   /*  let firstList = JSON.parse(await rediss.getAsync(first));
+    let secondList = JSON.parse(await rediss.getAsync(second)); */
+
+
     const mostPopular = [];
     const priceReduction = [];
 
     let IdLists;
-    let type = 'microservice';
-    let allIds = []
 
-
-    if(firstList == null ||firstList == undefined || secondList == null || secondList == undefined){
+  /*   if(firstList == null ||firstList == undefined || secondList == null || secondList == undefined){
 
         IdLists = await cacheInitialize(); 
         firstList = IdLists[0];
         secondList = IdLists[1];
-    }
-
-    /** Consulta a Api de Catalogo, mas sem ID, para tornar o processo mais veloz
-     * optei por trazer a lista inteira de produtos e guarda-la em cache, dessa
-     * forma é evitado que precise ser feito uma busca por cada id dentro da API,
-     * tentei fazer dessa forma mas é simplesmente inviavel. Para funcionar criei
-     * um novo type chamado microservice e passo o id como 0 (poderia ser qualquer id
-     * pois o campo que será o identificador é  o type)
-     */
-
-    const info = await axios.get('http://localhost:3001/api/products/'+ 0 + '/' + type)
-        .then((sucesso)=>{
-                return sucesso.data
-        })
-        .catch((erro)=>{
-                 return erro
-        }) 
-
-        console.log(info)
+    } */
 
 
+    //const validate = await rediss.getAsync(allProducts);
+
+    
+    let products =  await callApi();
+
+    console.log(products)
+
+    const fisrtList = await axios.get('https://wishlist.neemu.com/onsite/impulse-core/ranking/mostpopular.json');
 
 
-    /* const delay = ms => new Promise(res => setTimeout(res, ms));
+    console.log(fisrtList)
 
-    await Promise.all(firstList.map(async (mostPopularId) => {
 
-        await delay(3000);
+    /* await Promise.all(firstList.map((list) => {
 
         
+        products.map((prod)=>{
+            console.log(prod)
+         if(prod.id === list){
+            mostPopular.push(prod)
+         }
 
-        if(info.sucess){
-            if(info.product.status === 'AVAILABLE'){
-                mostPopular.push(info.product)
-            }
-        }
-        }))  
-
-
-    await Promise.all(secondList.map(async (reductionId) => {
-             
-        await delay(1500);
-        const sinfo = await axios.get('http://localhost:3001/api/products/'+ reductionId.recommendedProduct.id + '/' + type)
-        .then((sucesso)=>{
-                return sucesso.data
         })
-        .catch((erro)=>{
-                return erro
-        }) 
-    
-        if(sinfo.sucess){
-            if(sinfo.product.status === 'AVAILABLE'){
-                priceReduction.push(sinfo.product)
-            }
-        }
-    }))    */
+        
+        
+    }))   */
 
-        return res.json(delta);
+
+    
+
+    /* await Promise.all(secondList.map(async (products) => {
+
+
+
+        
+    }))
+
+     */
+
+
+        return "nada"
 
         //return res.json({list_1: mostPopular, list_2:priceReduction})
     }
